5ae7f39920266d175d9790cffc6582f9a6c7385f
monsteronfire <monsteronfire@gmail.com>
2016-04-02 09:19:38 +0800

    Add artists and songs route

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('artists', function() {
+    this.route('songs', { path: ':slug' });
+  });
 });
 
 export default Router;
